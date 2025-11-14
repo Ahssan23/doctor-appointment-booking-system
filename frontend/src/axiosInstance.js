f7c1ddcc1450a777ff2b20963e591e9a70c57856
Ahssan23 <ahssan180sx@gmail.com>
2025-11-14 19:29:02 +0500

    Update axiosInstance.js

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // make sure your backend URL is correct
+  baseURL: `${import.meta.env.VITE_API_URL}`, // make sure your backend URL is correct
 });
 
 axiosInstance.interceptors.request.use(
